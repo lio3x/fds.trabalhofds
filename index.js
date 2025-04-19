27293cb52785ef406f2e6b340dfd3bfc0bf2a160
lio3x <64290576+lio3x@users.noreply.github.com>
2025-04-19 20:50:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mensajem = document.querySelector("#mensagem");
+let resposta=document.querySelector("#resposta");
+let valor ="Não é porque seu sonho ainda não se realizaram que você deve parar de sonhar";
+
+
+mensajem.addEventListener("click",evento);
+
+function evento(){
+resposta.innerHTML = valor;
+setTimeout (()=>{resposta.innerHTML=''},3000);
+}
